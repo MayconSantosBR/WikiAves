db61a71211b7f52b520616e36e002717ebe4a482
maycon.santos.questor <maycon.santos.questor@outlook.com>
2023-07-19 09:30:54 -0300

    Initialize project for recognition

---
+++ WikiAves.AudioRecognizer.Test/Models/SpectogramPredictionEx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikiAves.AudioRecognizer.Test.Models
+{
+    public class SpectrogramPredictionEx
+    {
+        public string ImagePath;
+        public string Label;
+        public UInt32 PredictedLabel;
+        public float[] Score;
+    }
+}
